9c83ee7aa3adf720c2ebccaabce81a42198ef63e
Mr-Rohit-Mishra <rohitmishraholy@gmail.com>
2023-12-14 18:40:08 +0530

    added components navbar,news,newsitem

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    return (
+        <>
+            <div className="newsheading"><p>This is a news Item</p></div>
+      </>
+    )
+  }
+}
