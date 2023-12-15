6a8fa889d08bc52f3c4b1e2d7d5062165f00dbe7
Mr-Rohit-Mishra <rohitmishraholy@gmail.com>
2023-12-15 12:50:52 +0530

    added news api and it is working correctly and also moving to complete news on clicking read more

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description,imgUrl,newsUrl} = this.props;
     return (
         <>
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                      <img src="..." className="img-fluid rounded-start" alt="..."/>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                          <h5 className="card-title">{title}</h5>
-                          <p className="card-text">{description}</p>
-                          <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <div className="card mb-3">
+              <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={imgUrl} className="img-fluid rounded-start" alt="..."/>
+                  </div>
+                  <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href ={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </>
     )
   }
